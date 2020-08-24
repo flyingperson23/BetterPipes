f3624ac62d65163a68b98ba5e6fa82c206e17849
flyingperson23 <flyingperson23@gmail.com>
2020-08-24 10:43:33 -0500

    bump version number

--- src/main/java/flyingperson/BetterPipes/BetterPipes.java
+++ src/main/java/flyingperson/BetterPipes/BetterPipes.java
@@ -30,7 +30,7 @@ public class BetterPipes
 {
     public static final String MODID = "betterpipes";
     public static final String NAME = "Better Pipes";
-    public static final String VERSION = "0.5";
+    public static final String VERSION = "0.6";
 
     public static Logger logger;
 
